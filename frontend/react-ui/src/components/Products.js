c28f5089e71f761abdf094a2bd292878f32fd65d
Hunter White <hpw6813@gmail.com>
2025-05-29 14:39:23 -0400

    building the add product modal

--- frontend/react-ui/src/components/Products.js
+++ frontend/react-ui/src/components/Products.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Table from '@mui/material/Table';
+import { TextField } from '@mui/material';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
@@ -21,6 +22,14 @@ export default function Products() {
     const [products, setProducts] = useState(null)
     const [deleteID, setDeleteID] = useState(null)
     const [confirmOpen, setConfirmOpen] = useState(false)
+    const [newProduct, setNewProduct] = useState({
+        id:'',
+        name: '',
+        description: '',
+        brand: '',
+        acquisitionDate: '',
+        price: ''
+    })
 
     const handleConfirmOpen = (id) => {
         setDeleteID(id)
@@ -41,6 +50,28 @@ export default function Products() {
             alert('There was an error deleting the product please try again');
         }
     }
+    const handleChange = (e) => {
+        setNewProduct({...newProduct, [e.target.name]: e.target.value});
+    }
+    const handleAddProduct = async () => {
+        try{
+            const response = await axios.post('http://localhost8080/products', {
+                ...newProduct, 
+                price: parseFloat(newProduct.price)
+            });
+            setProducts([...products, response.data])
+            setNewProduct({
+                id:'',
+                name: '',
+                description: '',
+                brand: '',
+                acquisitionDate: '',
+                price: ''
+            });   
+        }catch (error){
+            console.log('There was an error adding the product!', error)
+        }
+    }
 
 
     useEffect(() => {
@@ -55,8 +86,13 @@ export default function Products() {
     display ='flex'
     justifyContent ='center'
     alignItems = 'center'
-    height ='75'>
+    height ='120vh'>
     <TableContainer component={Paper} style ={{width:'70%'}}>
+        <Box display = 'flex' justifyContent='flex-start'>
+        <Button variant = 'contained'>
+            Add New 
+        </Button>
+        </Box>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -121,6 +157,91 @@ export default function Products() {
             </Button>
             </DialogActions>
         </Dialog>
+        {/* Modal Dialog for Adding New Product */}
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Add New Product</DialogTitle>
+        <DialogContent>
+          <select
+            style={{
+              width: '100%',
+              padding: '8px',
+              borderRadius: '4px',
+              border: '1px solid #ccc',
+              marginBottom: '15px',
+              fontSize: '16px'
+            }}
+            id="categoryid"
+            onChange={handleCategoryChange}
+            value={newProduct.categoryid}
+          >
+            <option>Choose...</option>
+            {categories.filter(c => c.title != null).map(category => (
+              <option key={category.id} value={category.id} >
+                {category.title}
+              </option>
+            ))}
+          </select>
+          <select
+            id="subcategoryid"
+            onChange={handleSubCategoryChange}
+            value={newProduct.subcategoryid}
+            style={{
+              width: '100%',
+              padding: '8px',
+              borderRadius: '4px',
+              border: '1px solid #ccc',
+              marginBottom: '15px',
+              fontSize: '16px'
+            }}
+          >
+            <option>Choose...</option>
+            {filteredSubCategories.map(subCategory => (
+              <option
+                key={subCategory.id} 
+                value={subCategory.id}
+              >
+                {subCategory.description}
+              </option>
+            ))}
+          </select>
+          <TextField
+            margin="dense"
+            name="title"
+            label="Product Title"
+            type="text"
+            fullWidth
+            value={newProduct.title}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="dense"
+            name="summary"
+            label="Summary"
+            type="text"
+            fullWidth
+            value={newProduct.sumary}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="dense"
+            name="content"
+            label="Content"
+            type="text"
+            fullWidth
+            value={newProduct.content}
+            onChange={handleChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleAddProduct} color="primary" variant="contained">
+            Add Product
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
+
   )
 }
